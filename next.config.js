1a036dfbe9a704784dc049fb50bcad3a18ec9ed4
wraparoundtheworld <robotoeuf@gmail.com>
2024-07-05 00:32:39 +0900

    5

--- next.config.js
+++ next.config.js
@@ -10,7 +10,10 @@ const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
   images: {
     disableStaticImages: true
-  }
+  },
+  output: 'export', // Add this line for static export
+  basePath: isProd ? '/test' : '',
+  assetPrefix: isProd ? '/test/' : '',
 };
 
 module.exports = withTM(withFonts(withImages(nextConfig)));
